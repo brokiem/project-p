e7e1adc50b260784bad5795a108afeece82fe661
brokiem <milyhqvideo@gmail.com>
2024-01-29 08:38:44 +0800

    Add struktur organisasi page

---
+++ pages/profil/struktur-organisasi.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+useHead({
+  title: 'Profil - SMK Negeri 2 Tabanan',
+  meta: [
+    { name: 'description', content: 'Terwujudnya peserta didik yang terdidik, cerdas, terampil, mandiri dan berdaya saing global' }
+  ],
+});
+</script>
+
+<template>
+  <div>
+    <!-- Page title -->
+    <Header title="Struktur Organisasi"></Header>
+
+    <div class="container mx-auto px-4 md:max-w-default">
+      <div class="flex justify-center">
+        <img src="/struktur-organisasi.png" width="862" height="644" alt="Struktur organisasi">
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>

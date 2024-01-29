163752cda40f2af68ec80db11e1c263666d3b6a3
brokiem <milyhqvideo@gmail.com>
2024-01-29 09:15:07 +0800

    Add fasilitas page

---
+++ pages/profil/fasilitas.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <!-- Page title -->
+  <Header title="Fasilitas Sekolah"></Header>
+
+  <div class="container mx-auto px-4 md:max-w-default py-10 space-y-4">
+    <!-- Card -->
+    <div class="flex flex-col lg:flex-row items-center w-full bg-white border border-gray-300 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+      <!-- Card image -->
+      <div class="flex flex-col justify-between leading-normal p-6">
+        <!-- Title -->
+        <h5 class="mb-2 text-xl font-medium text-black dark:text-white">
+          Bengkel Komputer
+        </h5>
+        <!-- Description -->
+        <p class="mt-1 text-base font-normal text-gray-700 dark:text-gray-400">
+          SMKN 2 Tabanan  memiliki 3 bengkel komputer utama lengkap dengan LAN beserta jaringan internet.
+          Ruangan sejuk dengan berpendingin AC untuk selalu menjaga suhu ruangan yang dilengkapi dengan
+          LCD Proyektor sehingga pelaksanaan kegiatan pembelajaran berlangsung dengan nyaman.
+        </p>
+      </div>
+    </div>
+
+    <!-- Card -->
+    <div class="flex flex-col lg:flex-row items-center w-full bg-white border border-gray-300 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+      <!-- Card image -->
+      <div class="flex flex-col justify-between leading-normal p-6">
+        <!-- Title -->
+        <h5 class="mb-2 text-xl font-medium text-black dark:text-white">
+          Studio Broadcast
+        </h5>
+        <!-- Description -->
+        <p class="mt-1 text-base font-normal text-gray-700 dark:text-gray-400">
+          Jurusan Multimedia disediakan studio broadcast untuk melaksanakan siaran baik secara online maupun offline,
+          dengan didukung peralatan yang profesional seperti tersedianya video mixer, audio mixer dan tata cahaya.
+        </p>
+      </div>
+    </div>
+
+    <!-- Card -->
+    <div class="flex flex-col lg:flex-row items-center w-full bg-white border border-gray-300 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+      <!-- Card image -->
+      <div class="flex flex-col justify-between leading-normal p-6">
+        <!-- Title -->
+        <h5 class="mb-2 text-xl font-medium text-black dark:text-white">
+          Studio Foto
+        </h5>
+        <!-- Description -->
+        <p class="mt-1 text-base font-normal text-gray-700 dark:text-gray-400">
+          Untuk Kompetensi Keahlian Multimedia disediakan juga ruangan studio photo yang digunakan untuk praktek
+          pengolahan gambar digital yang dilengkapi dengan camera DSLR beserta tata cahaya dengan background
+          yang bisa diganti-ganti.
+        </p>
+      </div>
+    </div>
+
+    <!-- Card -->
+    <div class="flex flex-col lg:flex-row items-center w-full bg-white border border-gray-300 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+      <!-- Card image -->
+      <div class="flex flex-col justify-between leading-normal p-6">
+        <!-- Title -->
+        <h5 class="mb-2 text-xl font-medium text-black dark:text-white">
+          Dapur dan Ruang Saji
+        </h5>
+        <!-- Description -->
+        <p class="mt-1 text-base font-normal text-gray-700 dark:text-gray-400">
+          Kompetensi Keahlian Tata Boga disediakan dapur yang dilengkapi dengan alat-alat sesuai standar
+          sehingga dalam pengolahan makanan dan minuman dapat dilakukan dengan mudah. Selain itu pada
+          kompetensi keahlian ini juga dilengkapi dengan ruangan saji yang digunakan sebagai wadah
+          layaknya restoran di hotel berbintang.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>

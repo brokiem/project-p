204cee92c66eba1110c174841c2e870c94dd0ed8
broki <61258338+brokiem@users.noreply.github.com>
2024-02-01 15:23:03 +0800

    Implement ``/api/login` api

---
+++ server/api/login.ts
@@ -0,0 +1,43 @@
+import {PrismaClient} from '@prisma/client';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (event) => {
+  const {email, password_hash} = await readBody(event);
+
+  const user = await prisma.users.findFirst({
+    where: {email},
+  });
+
+  if (!user) {
+    return new Response(JSON.stringify({
+      success: false,
+      error: 'User not found',
+    }), {status: 404});
+  }
+
+  // Check if the password is valid using bcrypt
+  const isPasswordValid = await bcrypt.compare(password_hash, user.password_hash);
+
+  if (!isPasswordValid) {
+    return new Response(JSON.stringify({
+      success: false,
+      error: 'Invalid password',
+    }), {status: 401});
+  }
+
+  const { uuid, username } = user;
+  // Create a token using jwt
+  const jwtSecretKey = process.env.JWT_SECRET_KEY;
+  const token = jwt.sign({uuid, username, email}, jwtSecretKey!, {
+    algorithm: 'HS384',
+    expiresIn: 86400 // expires in 24 hours
+  });
+
+  return new Response(JSON.stringify({
+    success: true,
+    message: token,
+  }), {status: 200});
+});

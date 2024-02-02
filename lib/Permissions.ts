0603c6a235436863a118c76dd5b887631197b2cd
broki <61258338+brokiem@users.noreply.github.com>
2024-02-02 21:36:31 +0800

    Add a function to check if user has a permission

--- lib/Permissions.ts
+++ lib/Permissions.ts
@@ -1,3 +1,5 @@
+import type { User } from "~/lib/User";
+
 export enum Permissions {
   // Allows creation of news
   CREATE_NEWS = 1 << 0,
@@ -12,3 +14,14 @@ export enum Permissions {
   // Allows no permissions
   NONE = 1 << 5,
 }
+
+// Check if a user has a permission
+export const hasPermission = (user: User, permission: Permissions) => {
+  // If the user is an administrator, they have all permissions
+  if ((user.permissions & Permissions.ADMINISTRATOR) === Permissions.ADMINISTRATOR) {
+    return true;
+  }
+
+  // Otherwise, check if the user has the permission
+  return (user.permissions & permission) === permission;
+};

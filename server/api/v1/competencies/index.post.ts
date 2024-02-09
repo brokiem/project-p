0982b8d63494c8e78a541b7ab8e2493e58ee9a15
broki <61258338+brokiem@users.noreply.github.com>
2024-02-09 18:04:39 +0800

    Add permissions to manage competencies

--- server/api/v1/competencies/index.post.ts
+++ server/api/v1/competencies/index.post.ts
@@ -1,4 +1,6 @@
 import { prisma } from "~/prisma/db";
+import { User } from "~/utils/user";
+import { hasPermission, Permissions } from "~/utils/permissions";
 
 export default defineEventHandler(async (event) => {
     if (!event.context.isAuthenticated) {
@@ -6,6 +8,16 @@ export default defineEventHandler(async (event) => {
             success: false,
             error: "Unauthorized",
         }), { status: 401 }).json();
+    }
+
+    const user = event.context.user as User;
+
+    // Check if user has permission to delete competencies
+    if (!hasPermission(user.permissions, Permissions.CREATE_COMPETENCY)) {
+        return new Response(JSON.stringify({
+            success: false,
+            error: "You do not have permission to do this action!",
+        }), { status: 403 }).json();
     }
 
     const { title, description } = await readBody(event);
--- utils/permissions.ts
+++ utils/permissions.ts
@@ -1,5 +1,3 @@
-import type { user_attributes as UserAttributes } from "@prisma/client";
-
 export enum Permissions {
     // Allows creation of news
     CREATE_NEWS = 1 << 0,
@@ -15,15 +13,17 @@ export enum Permissions {
     NONE = 1 << 5,
     CREATE_EXTRACURRICULAR = 1 << 6,
     MANAGE_EXTRACURRICULAR = 1 << 7,
+    CREATE_COMPETENCY = 1 << 8,
+    MANAGE_COMPETENCY = 1 << 9,
 }
 
 // Check if a user has a permission
-export const hasPermission = (userAttributes: UserAttributes, permission: Permissions) => {
+export const hasPermission = (userPermissions: Permissions, permission: Permissions) => {
     // If the user is an administrator, they have all permissions
-    if ((userAttributes.permissions & Permissions.ADMINISTRATOR) === Permissions.ADMINISTRATOR) {
+    if ((userPermissions & Permissions.ADMINISTRATOR) === Permissions.ADMINISTRATOR) {
         return true;
     }
 
     // Otherwise, check if the user has the permission
-    return (userAttributes.permissions & permission) === permission;
+    return (userPermissions & permission) === permission;
 };

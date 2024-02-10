24f470f3b7d885cab4a1400abd4b8e4d8043bcea
broki <61258338+brokiem@users.noreply.github.com>
2024-02-10 21:00:26 +0800

    Fix `routeRules` by properly using rendering modes for routes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,14 @@ export default defineNuxtConfig({
         },
     },
     routeRules: {
-        "/": { prerender: true },
+        "/": { isr: 86400 },
+        "/profil/fasilitas": { prerender: true },
+        "/profil/logo": { prerender: true },
+        "/profil/sejarah": { prerender: true },
+        "/profil/struktur-organisasi": { prerender: true },
+        "/profil/visi-misi": { prerender: true },
+        "/kesiswaan/tatatertib": { prerender: true },
+        "/kesiswaan/ekstrakurikuler": { isr: 86400 },
     },
     runtimeConfig: {
         jwtSecretKey: process.env.JWT_SECRET_KEY,

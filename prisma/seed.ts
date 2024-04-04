f104cafcbcbcdb6cbd4ca65a0d38f25e3d1ab256
brokiem <milyhqvideo@gmail.com>
2024-04-04 10:41:11 +0800

    Update seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -60,10 +60,12 @@ async function main() {
                     roles: Roles.ADMIN,
                     articles: {
                         create: {
-                            image_url: "https://source.unsplash.com/random/1366x768/?landscape",
-                            title: "This is a news title!",
-                            summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec elit ultricies ultricies.",
-                            content: "",
+                            image_url: "https://images.unsplash.com/photo-1472396961693-142e6e269027?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=768&ixid=MnwxfDB8MXxyYW5kb218MHx8bGFuZHNjYXBlfHx8fHx8MTcxMjE5Nzk5MA&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1366",
+                            title: "Rainy Preparations, Unexpected Punches, and Unraveling Truth",
+                            summary: "In this collection of intriguing scenarios, the characters find themselves facing unexpected challenges and decisions.",
+                            content: {
+                                "ops": [{ "insert": "It was going to rain. The weather forecast didn't say that, but the steel plate in his hip did. He had learned over the years to trust his hip over the weatherman. It was going to rain, so he better get outside and prepare.\n\nTerrance knew that sometimes it was simply best to stay out of it. He kept repeating this to himself as he watched the scene unfold. He knew that nothing good would come of him getting involved. It was far better for him to stay on the sidelines and observe. He kept yelling this to himself inside his head as he walked over to the couple and punched the man in the face.\n\nDave wasn't exactly sure how he had ended up in this predicament. He ran through all the events that had lead to this current situation and it still didn't make sense. He wanted to spend some time to try and make sense of it all, but he had higher priorities at the moment. The first was how to get out of his current situation of being naked in a tree with snow falling all around and no way for him to get down.\n\nAfter hunting for several hours, we finally saw a large seal sunning itself on a flat rock. I took one of the wooden clubs while Larry took the longer one. We slowly snuck up behind the seal until we were close enough to club it over its head. The seal slumped over and died. This seal would help us survive. We could eat the meat and fat. The fat could be burned in a shell for light and the fur could be used to make a blanket. We declared our first day of hunting a great success.\n\nFinding the truth wouldn't be easy, that's for sure. Then there was the question of whether or not Jane really wanted to know the truth. That's the thing that bothered her most. It wasn't the difficulty of actually finding out what happened that was the obstacle, but having to live with that information once it was found.\n" }],
+                            },
                             type: ArticleType.NEWS,
                             flags: ArticleFlags.IS_PUBLISHED,
                         },
@@ -92,10 +94,12 @@ async function main() {
                     roles: Roles.ADMIN,
                     articles: {
                         create: {
-                            image_url: "https://source.unsplash.com/random/1366x768/?landscape",
-                            title: "This is an announcement title!",
-                            summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec elit ultricies ultricies.",
-                            content: "",
+                            image_url: "https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=768&ixid=MnwxfDB8MXxyYW5kb218MHx8bGFuZHNjYXBlfHx8fHx8MTcxMjE5Nzk3Mw&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1366",
+                            title: "Ethical Dilemmas, Nostalgic Trees, Perceptions of Tim",
+                            summary: "This collection of thought-provoking scenarios delves into various themes and emotions.",
+                            content: {
+                                "ops": [{ "insert": "\"Explain to me again why I shouldn't cheat?\" he asked. \"All the others do and nobody ever gets punished for doing so. I should go about being happy losing to cheaters because I know that I don't? That's what you're telling me?\"\n\nThe tree missed the days the kids used to come by and play. It still wore the tire swing the kids had put up in its branches years ago although both the tire and the rope had seen better days. The tree had watched all the kids in the neighborhood grow up and leave, and it wondered if there would ever be a time when another child played and laughed again under its branches. That was the hope that the tree wished every day as the swing gently swung empty in the wind.\n\nTime is all relative based on age and experience. When you are a child an hour is a long time to wait but a very short time when that’s all the time you are allowed on your iPad. As a teenager time goes faster the more deadlines you have and the more you procrastinate. As a young adult, you think you have forever to live and don’t appreciate the time you spend with others. As a middle-aged adult, time flies by as you watch your children grow up. And finally, as you get old and you have fewer responsibilities and fewer demands on you, time slows. You appreciate each day and are thankful you are alive. An hour is the same amount of time for everyone yet it can feel so different in how it goes by.\n\nHe stepped away from the mic. This was the best take he had done so far, but something seemed missing. Then it struck him all at once. Visuals ran in front of his eyes and music rang in his ears. His eager fingers went to work in an attempt to capture his thoughts hoping the results would produce something that was at least half their glory.\n\nThe paper was blank. It shouldn't have been. There should have been writing on the paper, at least a paragraph if not more. The fact that the writing wasn't there was frustrating. Actually, it was even more than frustrating. It was downright distressing.\n" }],
+                            },
                             type: ArticleType.ANNOUNCEMENT,
                             flags: ArticleFlags.IS_PUBLISHED,
                         },

73176cc8d24919da076a3f76ca33d37290059a38
brokiem <milyhqvideo@gmail.com>
2024-04-09 21:03:26 +0800

    Remove Osis from Navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -133,10 +133,6 @@ export default defineComponent({
                    class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow border dark:bg-gray-700">
                 <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefault">
                   <li>
-                    <a href="#"
-                       class="no-underline text-gray-700 block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Osis</a>
-                  </li>
-                  <li>
                     <NuxtLink to="/kesiswaan/tatatertib"
                               class="no-underline text-gray-700 block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
                       Tata Tertib

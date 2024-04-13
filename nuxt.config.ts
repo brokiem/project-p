85fd88498aa26443644a232b1d59615eac67b384
brokiem <milyhqvideo@gmail.com>
2024-04-13 16:45:01 +0800

    Add HTML lang id to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
     app: {
         head: {
+            htmlAttrs: { lang: "id" },
             charset: "utf-8",
             viewport: "width=device-width, initial-scale=1",
         },

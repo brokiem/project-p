79c18c68439ceaabb72f63d1d4be0d27bde44b12
brokiem <milyhqvideo@gmail.com>
2024-04-13 13:44:02 +0800

    Add loading indicator when searching users

--- pages/admin/pengguna/index.vue
+++ pages/admin/pengguna/index.vue
@@ -21,9 +21,12 @@ const token = useCookie("token");
 const searchInput = ref("");
 const users = ref<User[]>([]);
 const isSearched = ref(false);
+const isSearching = ref(false);
 
 async function searchUser() {
+  isSearching.value = true;
   const {message} = await $api.users.searchUsers(searchInput.value!, token.value!);
+  isSearching.value = false;
   users.value = message.users;
   isSearched.value = true;
 }
@@ -52,29 +55,35 @@ async function searchUser() {
         <div>
           <!-- Search input -->
           <div class="mb-4">
-            <p class="mb-1">Silahkan cari nama pengguna untuk memulai.</p>
-            <form @submit.prevent="searchUser">
-              <input v-model="searchInput" class="border-2 border-gray-300 bg-white h-10 px-3 w-80 rounded-md text-sm focus:outline-none"
-                     type="search" name="search" placeholder="Cari pengguna" required>
-            </form>
+            <p class="mb-1">Silahkan cari pengguna untuk memulai.</p>
+            <div class="flex flex-row">
+              <form @submit.prevent="searchUser">
+                <input v-model="searchInput" class="border-2 border-gray-300 bg-white h-10 px-3 w-80 rounded-md text-sm focus:outline-none"
+                       type="search" name="search" placeholder="Cari pengguna" required>
+              </form>
+
+              <div v-if="isSearching" class="lds-dual-ring mt-1 ml-4"></div>
+            </div>
 
             <!-- Users list -->
             <div class="mt-7">
-              <ul>
-                <li v-for="user in users" :key="user.user_uuid" class="mb-2">
-                  <div class="flex justify-between items-center border px-3 py-2 rounded-md">
-                    <div>
-                      <p class="font-medium">{{ user.username }}</p>
-                      <p>{{ user.users.email }}</p>
+              <div v-if="!isSearching">
+                <ul>
+                  <li v-for="user in users" :key="user.user_uuid" class="mb-2">
+                    <div class="flex justify-between items-center border px-3 py-2 rounded-md">
+                      <div>
+                        <p class="font-medium">{{ user.username }}</p>
+                        <p>{{ user.users.email }}</p>
+                      </div>
+                      <NuxtLink :to="`/admin/pengguna/${user.user_uuid}/edit`" class="transition-all duration-300 bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Edit
+                      </NuxtLink>
                     </div>
-                    <NuxtLink :to="`/admin/pengguna/${user.user_uuid}/edit`" class="transition-all duration-300 bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                      Edit
-                    </NuxtLink>
-                  </div>
-                </li>
-              </ul>
+                  </li>
+                </ul>
 
-              <p v-if="isSearched && users.length === 0" class="mt-4 font-medium text-lg">Tidak ada pengguna yang ditemukan dengan kata kunci tersebut.</p>
+                <p v-if="isSearched && users.length === 0" class="mt-4 font-medium text-lg">Tidak ada pengguna yang ditemukan dengan kata kunci tersebut.</p>
+              </div>
             </div>
           </div>
         </div>
@@ -84,5 +93,34 @@ async function searchUser() {
 </template>
 
 <style scoped>
+.lds-dual-ring,
+.lds-dual-ring:after {
+  box-sizing: border-box;
+}
 
+.lds-dual-ring {
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+}
+
+.lds-dual-ring:after {
+  content: " ";
+  display: block;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  border: 4px solid currentColor;
+  border-color: currentColor transparent currentColor transparent;
+  animation: lds-dual-ring 600ms linear infinite;
+}
+
+@keyframes lds-dual-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
 </style>

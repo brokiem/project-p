0276bce4f673935f757737645c16508283b1e247
brokiem <milyhqvideo@gmail.com>
2024-04-13 15:52:34 +0800

    Change "Info PPDB" button url to announcements with search "Info PPDB"

--- pages/index.vue
+++ pages/index.vue
@@ -42,7 +42,7 @@ try {
 
           <div class="my-6"></div>
 
-          <a href="https://www.smkn2tabanan.sch.id/ppdb">
+          <a href="/artikel/pengumuman/cari?q=Info%20PPDB">
             <button type="button"
                     class="text-white bg-blue-600 hover:bg-blue-800 font-normal rounded-md text-normal px-4 py-1.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:ring-blue-800 transition duration-200">
               INFO PPDB

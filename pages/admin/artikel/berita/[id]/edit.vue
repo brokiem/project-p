bd733820a256e926ec228a44d50c7b311b833d04
brokiem <milyhqvideo@gmail.com>
2024-04-15 15:40:34 +0800

    Allow article title up to 120 characters intead of 60

--- pages/admin/artikel/berita/[id]/edit.vue
+++ pages/admin/artikel/berita/[id]/edit.vue
@@ -393,7 +393,7 @@ function deleteArticle() {
     <input id="article-title" v-model="news.title" class="rounded-md transition duration-50 block w-full p-2 mb-4 text-sm text-gray-900 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
            placeholder="Judul artikel"
            required type="text"
-           maxlength="60"
+           maxlength="120"
     >
 
     <!-- Article summary input -->
